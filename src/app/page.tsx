61fd3d7f8c6ed15bebe4a9413b7c6536e2e9f6ac
Jordan-92 <83961261+Jordan-92@users.noreply.github.com>
2025-03-03 23:53:57 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
-import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
